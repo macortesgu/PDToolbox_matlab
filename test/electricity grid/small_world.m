8761e736928abc1e50a19e519ed08ffa9cebc49e
macg- <macg-@DESKTOP-0G10A8H>
2016-11-15 10:29:23 -0500

    small world m file

---
+++ test/electricity grid/small_world.m
@@ -0,0 +1,4 @@
+h4 = WattsStrogatz(500,25,0.1);
+plot(h4,'NodeColor','k','EdgeAlpha',0.1);
+title('Grafo Watts-Strogatz with $N = 200$ nodos, $K = 25$, and $\beta = 1$', ...
+    'Interpreter','latex')
